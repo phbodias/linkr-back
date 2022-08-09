06b9f054f5a009e0bf065392fc3381cddf0cb2ad
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-08-09 14:55:16 -0300

    authRoutes

--- src/middlewares/validateSchemas.js
+++ src/middlewares/validateSchemas.js
@@ -2,7 +2,7 @@ export default function (schema) {
     return (req,res,next) => {
         const validation = schema.validate(req.body);
         if(validation.error){
-            return res.status(422).send(validation.error[0].message)
+            return res.status(422).send(validation.error.message)
         } else {
             next()
         }
