30b0f82fefa8719a706b868995ea1d94e7c14000
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-08-15 15:23:41 -0300

    like route

--- src/repositories/likesRepository.js
+++ src/repositories/likesRepository.js
@@ -1,29 +1,27 @@
 import connection from "../dbStrategy/database.js";
 
-export async function getLikeByPostId (postId) {
-    return await connection.query(
-        `SELECT users.name 
+export async function getLikeByPostId(postId) {
+  return await connection.query(
+    `SELECT users.name 
         FROM likes
         JOIN users ON users.id=likes."userLikedId"
         WHERE likes."postId"=$1`,
-        [postId]
-        )
+    [postId]
+  );
 }
 
-export async function deleteLikeLink(id){
-    return await connection.query(
-        'DELETE FROM likes WHERE "postId"=$1',
-        [id]
-    )
+export async function deleteLikeLink(id) {
+  return await connection.query('DELETE FROM likes WHERE "postId"=$1', [id]);
 }
 
 export async function likePost(userId, postId) {
-    try {
-      return await connection.query(
-        'INSERT INTO likes ("postId", "userLikedId") VALUES ($1, $2)',
-        [postId, userId]
-      );
-    } catch (e) {
-      return res.status(500).send("erro no repo");
-    }
+  try {
+    await connection.query(
+      'INSERT INTO likes ("postId", "userLikedId") VALUES ($1, $2)',
+      [postId, userId]
+    );
+    return;
+  } catch (e) {
+    return res.status(500).send("erro no repo");
   }
+}
