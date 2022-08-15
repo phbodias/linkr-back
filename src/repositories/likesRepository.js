60981cf59f2e36c7eba0753546726ecb5255706f
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-08-15 15:35:44 -0300

    user liked id

--- src/repositories/likesRepository.js
+++ src/repositories/likesRepository.js
@@ -2,7 +2,7 @@ import connection from "../dbStrategy/database.js";
 
 export async function getLikeByPostId(postId) {
   return await connection.query(
-    `SELECT users.name 
+    `SELECT users.name, users.id 
         FROM likes
         JOIN users ON users.id=likes."userLikedId"
         WHERE likes."postId"=$1`,
