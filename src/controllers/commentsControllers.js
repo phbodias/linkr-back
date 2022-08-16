e3b21f07ff6305ed335b0c60a5b2acc300b17953
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-08-16 20:15:27 -0300

    feat: add /GET /comments/:id route to get all comments of a single post

--- src/controllers/commentsControllers.js
+++ src/controllers/commentsControllers.js
@@ -20,3 +20,17 @@ export async function createComment(req,res){
         return res.status(500).send('Error on createComment function on commentsControllers.js file');
     }
 }
+
+export async function listCommentsOfPost(_req,res){
+    const {id} = res.locals;
+    try{
+        const {rows: existPost} = await getOnePostById(id);
+        if(existPost.length===0) return res.status(404).send("There is no post whith this Id");
+        const comments = await commentsRepository.getCommentsOfPostById(id);
+        if(!comments) return res.status(500).send("Something went wrong on getting the comments");
+        return res.status(200).send({comments, commentsCount: comments.length}); 
+    }catch(err){
+        console.log(err);
+        return res.status(500).send('Error on listCommentsOfPost function on commentsControllers.js file');
+    }
+}
--- src/repositories/commentsRepository.js
+++ src/repositories/commentsRepository.js
@@ -10,6 +10,16 @@ export async function insertComments(userId, postId, text){
     return rowCount;
 }
 
+export async function getCommentsOfPostById(id){
+    const {rows:results} = await connection.query(`
+    SELECT "userId", text
+    FROM comments
+    WHERE "postId"=$1
+	ORDER BY "createdAt" DESC;`, [id]);
+    return results;
+}
+
 export const commentsRepository = {
     insertComments,
+    getCommentsOfPostById,
 }
